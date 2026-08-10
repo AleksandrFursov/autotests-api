e97d303b8d7354ed9d6139bb59e90409e4404dae
AleksandrFursov <sasha.fursof@gmail.com>
2026-08-10 15:34:39 +0700

    10.3 Добавление Allure шагов в автотесты

--- clients/authentication/authentication_client.py
+++ clients/authentication/authentication_client.py
@@ -1,9 +1,4 @@
-from httpx import Response
-
-from clients.api_client import APIClient
-from clients.authentication.authentication_schema import RefreshRequestSchema, LoginRequestSchema, \
-    LoginResponseSchema
-from clients.public_http_builder import get_public_http_client
+import allure
 from httpx import Response
 
 from clients.api_client import APIClient
@@ -17,6 +12,7 @@ class AuthenticationClient(APIClient):
     Клиент для работы с /api/v1/authentication
     """
 
+    @allure.step("Authenticate user")
     def login_api(self, request: LoginRequestSchema) -> Response:
         """
         Метод выполняет аутентификацию пользователя.
@@ -26,6 +22,7 @@ class AuthenticationClient(APIClient):
         """
         return self.post("/api/v1/authentication/login", json=request.model_dump(by_alias=True))
 
+    @allure.step("Refresh authentication token")
     def refresh_api(self, request: RefreshRequestSchema) -> Response:
         """
         Метод обновляет токен авторизации.
